6bdd3f28f843ff09ced644a2496954f3a5ec303a
Endzhe Galieva <endzhe91@gmail.com>
2021-09-06 00:25:15 +0300

    add music, news, settings components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-// import Profile from './components/Profile/Profile';
+import Profile from "./components/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from './components/Music/Music';
+import Settings from "./components/Settings/Settings";
 
-const App = () => {
+const App = (props) => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Navbar />
       <div className='app-wrapper-content'>
-        <Dialogs />
+         <Route path='/dialogs' component={Dialogs} />
+         <Route path='/profile' component={Profile} />
+         <Route path='/news' component={News} />
+         <Route path='/music' component={Music} />
+         <Route path='/settings' component={Settings} />
       </div>
-      {/*<Profile/>*/}
-    </div>);
+    </div>
+    </BrowserRouter>);
 }
 
 export default App;
---
+++ src/components/News/News.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from  '../News/News.module.css';
+
+const News = (props) => {
+  return (
+    <div>
+      News
+    </div>
+  );
+}
+export default News;
