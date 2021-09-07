76229dc81c28b25cfc3235a526c45e3e3eb3aed7
Endzhe Galieva <endzhe91@gmail.com>
2021-09-07 11:43:34 +0300

    UI - BLL

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -13,28 +13,45 @@ const DialogItem = (props) => {
 }
 
 const Message = (props) => {
-    return <div className={s.dialog}>{props.message}</div>
+  return <div className={s.dialog}>{props.message}</div>
 }
 
 const Dialogs = (props) => {
+
+  let dialogsData = [
+    {id: 1, name: "Dimych"},
+    {id: 2, name: "Andrey"},
+    {id: 3, name: "Sveta"},
+    {id: 4, name: "Sasha"},
+    {id: 5, name: "Viktor"},
+    {id: 6, name: "Valera"}
+  ]
+
+  let messagesData = [
+    {id: 1, message: "Hi"},
+    {id: 2, message: "How is your?"},
+    {id: 3, message: "Yo"},
+    {id: 4, message: "Yo"},
+    {id: 5, message: "Yo"}
+  ]
+
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name="Dimych" id="1"/>
-        <DialogItem name="Andrey" id="2"/>
-        <DialogItem name="Sveta" id="3"/>
-        <DialogItem name="Sasha" id="4"/>
-        <DialogItem name="Viktor" id="5"/>
-        <DialogItem name="Valera" id="6"/>
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
       </div>
       <div className={s.messages}>
-        <Message message="Hi"/>
-        <Message message="How is your?"/>
-        <Message message="Yo"/>
-        <Message message="Yo"/>
-        <Message message="Yo"/>
+        <Message message={messagesData[0].message}/>
+        <Message message={messagesData[1].message}/>
       </div>
     </div>
   );
 }
+
 export default Dialogs;
